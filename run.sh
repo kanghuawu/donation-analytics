f83bf85f358c2ea78a213d66c22b405855e8065d
Kang-Hua Wu <kang-hua.wu@sjsu.edu>
2018-02-12 21:56:08 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd src/DonationAnalytics
 # mvn clean package -Dmaven.test.skip=true # <- uncomment this line
-java -cp ./target/donation-analytics-1.0-SNAPSHOT.jar com.khwu.analytics.Main ../../input/itcont.txt ../../input/percentile.txt ../../output/repeat_donors.txt
+java -cp ./target/donation-analytics-1.0-SNAPSHOT.jar com.khwu.analytics.Main ../../input/itcont.txt ../../input/percentile.txt ../../output/percentile_by_zip_and_year.txt
